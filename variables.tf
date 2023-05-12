70897d888220f46877122f11b701143d89c9056d
Shomarri Romell Diaz <111364800+sdcloud92@users.noreply.github.com>
2023-05-12 02:13:53 +0100

    Update variables.tf
    This is a variables.tf file which defines input variables for our infrastructure-as-code (IaC) Terraform project.

--- variables.tf
+++ variables.tf
@@ -1,7 +1,13 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
 variable "instance_type" {
   type    = string
   default = "t2.micro"
 }
+
 
 variable "ami_id" {
   description = "AMI ID for Amazon Linux 2"
