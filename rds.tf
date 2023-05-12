549f4fd470a21f0aa171800f5afdfdc7a81e2c22
Shomarri Romell Diaz <111364800+sdcloud92@users.noreply.github.com>
2023-05-12 01:37:07 +0100

    Create rds.tf
    aws_db_instance: This resource is used to create an RDS MySQL instance with the specified configuration options. The following parameters are set:
    allocated_storage: The amount of storage to be allocated to the instance in GB.
    db_name: The name of the database to be created on the instance.
    engine: The database engine to use. In this case, it is MySQL.
    engine_version: The version of the database engine to use. In this case, it is MySQL version 5.7.
    instance_class: The instance type to be used for the RDS instance.
    username and password: The credentials to be used to access the instance. These are sensitive variables that will be provided in Terraform Cloud.
    vpc_security_group_ids: The security group to be attached to the instance.
    db_subnet_group_name: The name of the subnet group for the instance.
    skip_final_snapshot: Whether or not to create a final snapshot when the instance is deleted.
    aws_db_subnet_group: This resource is used to create a subnet group for the RDS instance. The following parameters are set:
    name: The name of the subnet group.
    subnet_ids: The IDs of the subnets in which the RDS instance will be launched.
    tags: Tags to be attached to the subnet group.

---
+++ rds.tf
@@ -0,0 +1,25 @@
+#create RDS MySQL Instance
+resource "aws_db_instance" "Project-dbinstance" {
+  allocated_storage = 20
+  db_name           = "Project-dbinstance"
+  engine            = "mysql"
+  engine_version    = "5.7"
+  instance_class    = "db.t2.micro"
+
+  #credentials will be added as sensitive variables in Terraform Cloud
+  username               = var.db_username
+  password               = var.db_password
+  vpc_security_group_ids = [aws_security_group.Project-mysql-sg.id]
+  db_subnet_group_name   = "db-subnet-grp"
+  skip_final_snapshot    = true
+}
+
+#subnet group for RDS instance
+resource "aws_db_subnet_group" "Project-db-subnet-grp" {
+  name       = "project-db-subnet-grp"
+  subnet_ids = [aws_subnet.Project-privsub1.id, aws_subnet.Project-privsub1.id]
+
+  tags = {
+    Name = "Project DB private subnet group"
+  }
+}
